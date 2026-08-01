5f5b22e219adb2eff5311de9a5452527fa8589af
priya2612 <priyagujar90@gmail.com>
2026-08-01 22:43:12 +0530

    feat: Implement jwt authentication filter and secure API's

--- Backend/StyleAura/src/main/java/com/styleaura/config/SecurityConfig.java
+++ Backend/StyleAura/src/main/java/com/styleaura/config/SecurityConfig.java
@@ -1,43 +1,60 @@
 package com.styleaura.config;
 
 import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.styleaura.jwt.JwtAuthenticationFilter;
+
 
 @Configuration
 public class SecurityConfig {
 
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+	public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+	    this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+	}
+	
+	
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+            throws Exception {
 
         http
                 .csrf(csrf -> csrf.disable())
 
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS))
+
                 .authorizeHttpRequests(auth -> auth
 
-                        // Swagger
                         .requestMatchers(
                                 "/swagger-ui/**",
-                                "/v3/api-docs/**"
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**",
+                                "/api/users/register",
+                                "/api/auth/login"
                         ).permitAll()
 
-                        // Registration
-                        .requestMatchers("/api/users/register", "/api/auth/login").permitAll()
-
-                        // Everything else requires login
                         .anyRequest().authenticated()
                 )
 
-                .httpBasic(Customizer.withDefaults());
+                .addFilterBefore(
+                        jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
